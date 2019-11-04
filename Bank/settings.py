055550633b35d781f1ce9a0ce558994b5d205449
xgf <xgf_aon@163.com>
2019-11-04 15:44:47 +0800

    update: 重拾 跑通

--- Bank/settings.py
+++ Bank/settings.py
@@ -90,24 +90,24 @@ WSGI_APPLICATION = 'Bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "bbs",
-        'USER': 'root',
-        'PASSWORD': "123456",
-        'HOST': "127.0.0.1",
-        # 'CHARSET':'utf8',
-        'OPTIONS': { 'init_command': 'SET default_storage_engine=INNODB;' }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "bbs",
+#         'USER': 'root',
+#         'PASSWORD': "123456",
+#         'HOST': "127.0.0.1",
+#         # 'CHARSET':'utf8',
+#         'OPTIONS': { 'init_command': 'SET default_storage_engine=INNODB;' }
+#     }
+# }
 
 
 
